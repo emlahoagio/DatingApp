6c9727d47454a2073ef8ea38f810a33af07aea2c
quantmdo <quandtmdo-kms@prxcision.com>
2023-11-30 15:55:30 +0700

    End of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'Dating app ';
+  users: any;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (res) => (this.users = res),
+      error: () => console.log(console.error()),
+      complete: () => console.log('request has completed'),
+    });
+  }
+}
